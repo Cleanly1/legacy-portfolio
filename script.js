9f67532a03648aa0a0b33a0a410f2806adc98056
Cleanly <oskar.turesson99@gmail.com>
2019-09-25 21:12:38 +0200

    Added a google font

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 var video = document.getElementById('player');
-var social = document.getElementsByClassName("socialLinks")
+var social = document.getElementsByClassName("socialLinks");
+var dateYear = new Date();
+document.getElementById("date").innerHTML = dateYear.getFullYear();
+
 
 function madness() {
   document.body.style.animation = "backg 1s infinite";
@@ -7,8 +10,8 @@ function madness() {
   document.getElementById('nick').style.animation = "colorT 1s infinite";
   social[0].style.animation = "rotateImage 0.5s infinite";
   social[1].style.animation = "rotateImageReverse 0.5s infinite";
-  
   video.play();
+
 };
 
 function stopMadness() {
@@ -18,4 +21,5 @@ function stopMadness() {
   document.getElementById('nick').style.animation = "none";
   social[0].style.animation = "none";
   social[1].style.animation = "none";
+
 }
