3c0a0cb676f36cb9381404a80f19775915383344
Cleanly <oskar.turesson99@gmail.com>
2019-09-25 20:07:00 +0200

    Added a seperate javascript file for easier overview of the files

---
+++ script.js
@@ -0,0 +1,19 @@
+var video = document.getElementById('player');
+var social = document.getElementsByClassName("socialLinks")
+function madness() {
+  document.body.style.animation = "backg 1s infinite";
+  document.getElementById('name').style.animation = "colorT 1s infinite";
+  document.getElementById('nick').style.animation = "colorT 1s infinite";
+  social[0].style.animation = "rotateImage 0.5s infinite";
+  social[1].style.animation = "rotateImageReverse 0.5s infinite";
+  video.play();
+};
+
+function stopMadness() {
+  video.pause();
+  document.body.style.animation = "none";
+  document.getElementById('name').style.animation = "none";
+  document.getElementById('nick').style.animation = "none";
+  social[0].style.animation = "none";
+  social[1].style.animation = "none";
+}
