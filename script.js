ebc719b340f87d5c20b37b2004443e9c373271cc
Oskar Turesson <OskarT@Anonymous.local>
2019-09-30 15:33:53 +0200

    Added a time function that updates every second

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 var video = document.getElementById('player');
 var social = document.getElementsByClassName("socialLinks");
-var dateYear = new Date();
-document.getElementById("date").innerHTML = dateYear.getFullYear();
+var myVar = setInterval(theTime, 1000);
+mytimer();
 
 
 function madness() {
   document.body.style.animation = "backg 1s infinite";
   document.getElementById('name').style.animation = "colorT 1s infinite";
   document.getElementById('nick').style.animation = "colorT 1s infinite";
+  document.getElementById('time').style.animation = "colorT 1s infinite";
   social[0].style.animation = "rotateImage 0.25s ease-in-out infinite";
   social[1].style.animation = "rotateImageReverse 0.25s ease-in-out infinite";
   video.play();
@@ -19,7 +20,12 @@ function stopMadness() {
   document.body.style.animation = "none";
   document.getElementById('name').style.animation = "none";
   document.getElementById('nick').style.animation = "none";
+  document.getElementById('time').style.animation = "none";
   social[0].style.animation = "none";
   social[1].style.animation = "none";
 
-}
+};
+
+function theTime() {
+var time = new Date();
+document.getElementById("time").innerHTML = time.toLocaleTimeString(); };
