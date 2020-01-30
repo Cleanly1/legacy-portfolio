91122689f3768f9434c8eb36353a48f5f54f2d81
Cleanly <oskar.turesson99@gmail.com>
2020-01-30 17:43:02 +0100

    added projects

--- script.js
+++ script.js
@@ -1,33 +1,48 @@
-var video = document.getElementById('player');
+var video = document.getElementById("player");
 var social = document.getElementsByClassName("socialLinks");
 var myVar = setInterval(theTime, 1000);
 
-function madness() {
-  document.body.style.animation = "backg 1s infinite";
-  document.getElementById('name').style.animation = "colorT 1s infinite";
-  document.getElementById('nick').style.animation = "colorT 1s infinite";
-  document.getElementById('time').style.animation = "colorT 1s infinite";
-  for (var i = 0; i < (social.length-1); i++) {
-    social[i].style.animation = "rotateImageReverse 0.25s ease-in-out infinite alternate forwards";
-  }
-  social[2].style.animation = "rotateImage 0.25s ease-in-out infinite alternate forwards";
-  video.play();
+const projects = document.querySelectorAll(".project");
 
-};
+projects.forEach(function(project) {
+    const projectLink = project.querySelector(".projectLink");
+    project.addEventListener("mouseover", function(event) {
+        projectLink.classList.add("linkHover");
+        project.classList.add("projectHover");
+        console.log(event);
+    });
+    project.addEventListener("mouseout", function(event) {
+        projectLink.classList.remove("linkHover");
+        project.classList.remove("projectHover");
+    });
+});
+
+function madness() {
+    document.body.style.animation = "backg 1s infinite";
+    document.getElementById("name").style.animation = "colorT 1s infinite";
+    document.getElementById("nick").style.animation = "colorT 1s infinite";
+    document.getElementById("time").style.animation = "colorT 1s infinite";
+    for (var i = 0; i < social.length - 1; i++) {
+        social[i].style.animation =
+            "rotateImageReverse 0.25s ease-in-out infinite alternate forwards";
+    }
+    social[2].style.animation =
+        "rotateImage 0.25s ease-in-out infinite alternate forwards";
+    video.play();
+}
 
 function stopMadness() {
-  video.pause();
-  document.body.style.animation = "none";
-  document.getElementById('name').style.animation = "none";
-  document.getElementById('nick').style.animation = "none";
-  document.getElementById('time').style.animation = "none";
-  for (var i = 0; i < social.length; i++) {
-    social[i].style.animation = "none";
-  }
-
-};
+    video.pause();
+    document.body.style.animation = "none";
+    document.getElementById("name").style.animation = "none";
+    document.getElementById("nick").style.animation = "none";
+    document.getElementById("time").style.animation = "none";
+    for (var i = 0; i < social.length; i++) {
+        social[i].style.animation = "none";
+    }
+}
 
 function theTime() {
-  var time = new Date();
-  document.getElementById("time").innerHTML = time.toLocaleTimeString();
-};
+    var time = new Date();
+    document.getElementById("time").innerHTML = time.toLocaleTimeString();
+}
